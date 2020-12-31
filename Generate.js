4d2e48b12ff45cc1e25c44f73aed8b0c31c85771
JackRyan_Smith <JackRyan3d@gmail.com>
2020-12-30 17:44:06 -0800

    update

---
+++ Generate.js
@@ -0,0 +1,57 @@
+function generateMarkdown(data) {
+    let l = data.license.replace(" ", "%20");
+    return `
+# ${data.title} ![Github license](https://img.shields.io/badge/license-${l}-blue)
+​
+## Description
+​
+${data.description}
+​
+## Table of Contents
+​
+* [Installation](#installation)
+​
+* [Usage](#usage)
+​
+* [License](#license)
+​
+* [Contributing](#contributing)
+​
+* [Tests](#tests)
+​
+* [Questions](#questions)
+​
+## Installation
+​
+To install necessary dependencies, run the following command:
+\`\`\`
+${data.installation}
+\`\`\`
+​
+## Usage
+​
+${data.usage}
+​
+## License
+​
+This project is licensed under the ${data.license}
+​
+## Contributing
+​
+${data.contribute}
+​
+## Tests
+​
+To run tests, run the following command:
+\`\`\`
+${data.tests}
+\`\`\`
+​
+## Questions 
+​
+If you have any questions about the repo, open an issue or contact me directly at
+${data.githubEmail}. You can find more of my work at [${data.githubUsername}](https://github.com/${data.githubUsername}).
+    `;
+}
+​
+module.exports = generateMarkdown;
